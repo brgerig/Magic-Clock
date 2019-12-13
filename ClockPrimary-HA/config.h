ef044e88a2cb7162a377ab4c4fc504bac7ee6e3a
brgerig <brgerig@taylor.edu>
2019-12-13 14:48:08 -0500

    Create config.h

---
+++ ClockPrimary-HA/config.h
@@ -0,0 +1,9 @@
+#define SSID                          "WiFiSSID"
+#define PASSWORD                      "WiFiPWD"
+
+// MQTT Settings
+#define HOSTNAME                      "magic-clock"
+#define MQTT_SERVER                   "something.cloudmqtt.com"
+#define MQTT_PORT                     11111
+#define MQTT_USER                     "clockuser"
+#define MQTT_PASS                     "clockpass"
